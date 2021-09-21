a48b750e1cd49b5d27e7824ead6a009f22a55fd7
Andrew <andrew.narolsky@gmail.com>
2021-09-21 14:21:55 +0300

    add open & close methods. remove arrows functions

--- js/script.js
+++ js/script.js
@@ -1,60 +1,38 @@
-// const Modal = function(options) {
-//     this.selectors = document.querySelectorAll(options.selectors);
-//     this.body = document.querySelector('body');
-//
-//     this.init = () => {
-//         this.openModal();
-//         this.closeModal();
-//     }
-//
-//     this.openModal = function () {
-//         for (let $selector in this.selectors) {
-//             if (this.selectors.hasOwnProperty($selector)) {
-//                 this.selectors[$selector].addEventListener('click',  () => {
-//                     this.body.classList.add('open__modal');
-//                     let attr = this.selectors[$selector].getAttribute('data-modal');
-//                     let modal = document.querySelector(attr);
-//                     setTimeout(() => {
-//                         modal.classList.add('open__modal__window');
-//                     }, 350);
-//                 });
-//             }
-//         }
-//     }
-//
-//     this.closeModal = function () {
-//         let $closeButtons = document.querySelectorAll('.close-modal');
-//         for (let $closeButton in $closeButtons) {
-//             if ($closeButtons.hasOwnProperty($closeButton)) {
-//                 $closeButtons[$closeButton].addEventListener('click',  () => {
-//                     let attr = $closeButtons[$closeButton].getAttribute('data-modal');
-//                     let modal = document.querySelector(attr);
-//                     modal.classList.remove('open__modal__window');
-//                     setTimeout(() => {
-//                         this.body.classList.remove('open__modal');
-//                     }, 350);
-//                 });
-//             }
-//         }
-//     }
-// }
-
 const Modal = function(options) {
     this.selectors = document.querySelectorAll(options.selectors);
     this.body = document.querySelector('body');
     this.openModal();
     this.closeModal();
+
+    this.open = function (attr) {
+        this.body.classList.add('open__modal');
+        let $modal = document.querySelector(attr);
+        setTimeout(function () {
+            $modal.classList.add('open__modal__window');
+        }, 350);
+    }
+
+    this.close = function (attr) {
+        let $body = this.body;
+        let $modal = document.querySelector(attr);
+        $modal.classList.remove('open__modal__window');
+        setTimeout(function () {
+            $body.classList.remove('open__modal');
+        }, 350);
+    }
 }
 
 Modal.prototype.openModal = function () {
     for (let $selector in this.selectors) {
         if (this.selectors.hasOwnProperty($selector)) {
-            this.selectors[$selector].addEventListener('click',  () => {
-                this.body.classList.add('open__modal');
-                let attr = this.selectors[$selector].getAttribute('data-modal');
-                let modal = document.querySelector(attr);
-                setTimeout(() => {
-                    modal.classList.add('open__modal__window');
+            let $element = this.selectors[$selector];
+            let $body = this.body;
+            $element.addEventListener('click',  function () {
+                $body.classList.add('open__modal');
+                let attr = $element.getAttribute('data-modal');
+                let $modal = document.querySelector(attr);
+                setTimeout(function () {
+                    $modal.classList.add('open__modal__window');
                 }, 350);
             });
         }
@@ -65,12 +43,13 @@ Modal.prototype.closeModal = function () {
     let $closeButtons = document.querySelectorAll('.close-modal');
     for (let $closeButton in $closeButtons) {
         if ($closeButtons.hasOwnProperty($closeButton)) {
-            $closeButtons[$closeButton].addEventListener('click',  () => {
+            let $body = this.body;
+            $closeButtons[$closeButton].addEventListener('click',  function () {
                 let attr = $closeButtons[$closeButton].getAttribute('data-modal');
-                let modal = document.querySelector(attr);
-                modal.classList.remove('open__modal__window');
-                setTimeout(() => {
-                    this.body.classList.remove('open__modal');
+                let $modal = document.querySelector(attr);
+                $modal.classList.remove('open__modal__window');
+                setTimeout(function() {
+                    $body.classList.remove('open__modal');
                 }, 350);
             });
         }
